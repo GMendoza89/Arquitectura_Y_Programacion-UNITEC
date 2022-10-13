09db035ca1afe2b0f0c7272320d02388c5128b63
Gustavo David Mendoza Pinto <gustavo.mendoza.dp@gmail.com>
2022-10-13 18:30:37 -0500

    Se incorporan los ejemplos para comparar lenguaje compilado (c++) y lenguaje interpretado (Python)

---
+++ C++/contar.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char **argv){
+    int a = 0;
+    while(a < 999999999){
+        a = a+1;
+    }
+    cout<<"Termino la cuenta\n";
+   
+    return 0;
+}
---
+++ Python Scrips/contar.py
@@ -0,0 +1,4 @@
+a = 0
+while a < 999999999:
+    a = a+1
+print("Termino la cuenta\n")
